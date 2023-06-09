934b5660052d7f757b959cca430159d9b20921c8
ahmadmusyadada <ahmadmusyadadaminullah@gmail.com>
2023-06-09 14:17:00 +0700

    Update formulir.php

--- formulir.php
+++ formulir.php
@@ -80,9 +80,123 @@
         </div>
       </div>
       <div class="form-group">
-        <label class="col-sm-2 col-sm-2 control-label">Nama SD/MI </label>
+        <label class="col-sm-2 col-sm-2 control-label">Nama SD/MI Asal </label>
         <div class="col-sm-10">
-          <input type="number" class="form-control" name="nisn">
+          <input type="number" class="form-control" name="namasdmiasal">
+        </div>
+      </div>
+      <div class="form-group">
+        <label class="col-sm-2 col-sm-2 control-label">Alamat Sekolah Asal </label>
+        <div class="col-sm-10">
+          <input type="number" class="form-control" name="alamatsekolahasal">
+        </div>
+      </div>
+      <div class="form-group">
+        <label class="col-sm-2 col-sm-2 control-label">Nama lengkap Ayah</label>
+        <div class="col-sm-10">
+          <input type="number" class="form-control" name="namalengkapayah">
+        </div>
+      </div>
+      <div class="form-group">
+        <label class="col-sm-2 col-sm-2 control-label">Nama lengkap Ibu</label>
+        <div class="col-sm-10">
+          <input type="number" class="form-control" name="namalengkapibu">
+        </div>
+      </div>
+      <div class="form-group">
+        <label class="col-sm-2 col-sm-2 control-label">Alamat Ayah</label>
+        <div class="col-sm-10">
+          <input type="number" class="form-control" name="alamatayah">
+        </div>
+      </div>
+      <div class="form-group">
+        <label class="col-sm-2 col-sm-2 control-label">Alamat Ibu</label>
+        <div class="col-sm-10">
+          <input type="number" class="form-control" name="alamatibu">
+        </div>
+      </div>
+      <div class="form-group">
+        <label class="col-sm-2 col-sm-2 control-label">Telepon/HP</label>
+        <div class="col-sm-10">
+          <input type="number" class="form-control" name="teleponhportu">
+        </div>
+      </div>
+      <div class="form-group">
+        <label class="col-sm-2 col-sm-2 control-label">Pekerjaan Ayah</label>
+        <div class="col-sm-10">
+          <input type="number" class="form-control" name="pekerjaanAyah">
+        </div>
+      </div>
+      <div class="form-group">
+        <label class="col-sm-2 col-sm-2 control-label">Pekerjaan Ibu</label>
+        <div class="col-sm-10">
+          <input type="number" class="form-control" name="pekerjaanibu">
+        </div>
+      </div>
+      <div class="form-group">
+        <label class="col-sm-2 col-sm-2 control-label">Pendidikan Terakhir Ayah</label>
+        <div class="col-sm-10">
+          <input type="number" class="form-control" name="pendidikanterakhirayah">
+        </div>
+      </div>
+      <div class="form-group">
+        <label class="col-sm-2 col-sm-2 control-label">Pendidikan Terakhir Ibu</label>
+        <div class="col-sm-10">
+          <input type="number" class="form-control" name="pendidikanterakhiribu">
+        </div>
+      </div>
+      <div class="form-group">
+        <label class="col-sm-2 col-sm-2 control-label">Nama Ayah Wali</label>
+        <div class="col-sm-10">
+          <input type="number" class="form-control" name="namaayahwali">
+        </div>
+      </div>
+      <div class="form-group">
+        <label class="col-sm-2 col-sm-2 control-label">Nama Ibu Wali</label>
+        <div class="col-sm-10">
+          <input type="number" class="form-control" name="namaibuwali">
+        </div>
+      </div>
+      <div class="form-group">
+        <label class="col-sm-2 col-sm-2 control-label">Alamat Ayah Wali</label>
+        <div class="col-sm-10">
+          <input type="number" class="form-control" name="alamatayahwali">
+        </div>
+      </div>
+      <div class="form-group">
+        <label class="col-sm-2 col-sm-2 control-label">Alamat Ibu Wali</label>
+        <div class="col-sm-10">
+          <input type="number" class="form-control" name="alamatibuwali">
+        </div>
+      </div>
+      <div class="form-group">
+        <label class="col-sm-2 col-sm-2 control-label">Telepon/HP</label>
+        <div class="col-sm-10">
+          <input type="number" class="form-control" name="teleponwali">
+        </div>
+      </div>
+      <div class="form-group">
+        <label class="col-sm-2 col-sm-2 control-label">Pekerjaan Ayah Wali</label>
+        <div class="col-sm-10">
+          <input type="number" class="form-control" name="pekerjaanayahwali">
+        </div>
+      </div>
+      <div class="form-group">
+        <label class="col-sm-2 col-sm-2 control-label">Pekerjaan Ibu Wali</label>
+        <div class="col-sm-10">
+          <input type="number" class="form-control" name="pekerjaanibuwali">
+        </div>
+      </div>
+      <div class="form-group">
+        <label class="col-sm-2 col-sm-2 control-label">Pendidikan Terakhir Ayah Wali</label>
+        <div class="col-sm-10">
+          <input type="number" class="form-control" name="pendidikanterakhirayahwali">
+        </div>
+      </div>
+      <div class="form-group">
+        <label class="col-sm-2 col-sm-2 control-label">Pendidikan Terakhir Ibu Wali</label>
+        <div class="col-sm-10">
+          <input type="number" class="form-control" name="pendidikanterakhiribuwali">
         </div>
       </div>
       <div class="form-group">
