0812079b7c8fe6c005967a5a0d8eed5712ace9b0
ahmadmusyadada <ahmadmusyadadaminullah@gmail.com>
2023-06-21 13:20:39 +0700

    Update formulir_action.php

--- formulir_action.php
+++ formulir_action.php
@@ -18,42 +18,63 @@
         }
     }
 
-    $surat_dari = $_POST['suratdari'];
-    $no_surat = $_POST['nosurat'];
-    $tanggal_surat = $_POST['tanggalsurat'];
-    $diterima_tanggal = $_POST['diterimatanggal'];
-    $no_agenda = $_POST['noagenda'];
-    $sifat = $_POST['sifat'];
-    $sifat2 = $_POST['sifat2'];
-    $sifatfix = "";
-    $hal = $_POST['hal'];
-    $sifat_hal2c = $_POST['sifathal2c'];
-    $sifat_hal2 = $_POST['sifathal2'];
-    $sifat_hal_fix = "";
+    $nama_lengkap = $_POST['namalengkap'];
+    $nama_panggilan = $_POST['namapanggilan'];
+    $nomor_induk_asal = $_POST['nomorindukasal'];
+    $nisn = $_POST['nisn'];
+    $ttl = $_POST['ttl'];
+    $jenis_kelamin = $_POST['jeniskelamin'];
+    $agama = $_POST['agama'];
+    $anak_ke = $_POST['anakke'];
+    $status_anak_dalam_keluarga = $_POST['statusanakdalamkeluarga'];
+    $alamat = $_POST['alamat'];
+    $telepon_hp = $_POST['teleponhp'];
+    $di_kelas = $_POST['dikelas'];
+    $pada_tanggal = $_POST['padatanggal'];
+    $nama_sd_mi_asal = $_POST['namasdmiasal'];
+    $alamat_sekolah_asal = $_POST['alamatsekolahasal'];
+    $nama_lengkap_ayah = $_POST['namalengkapayah'];
+    $nama_lengkap_ibu = $_POST['namalengkapibu'];
+    $alamat_ayah = $_POST['alamatayah'];
+    $alamat_ibu = $_POST['alamatibu'];
+    $telepon_hp_ortu = $_POST['teleponhportu'];
+    $pekerjaan_ayah = $_POST['pekerjaanayah'];
+    $pekerjaan_ibu = $_POST['pekerjaanibu'];
+    $pendidikan_terakhir_ayah = $_POST['pendidikanterakhirayah'];
+    $pendidikan_terakhir_ibu = $_POST['pendidikanterakhiribu'];
+    $nama_ayah_wali = $_POST['namayahwali'];
+    $nama_ibu_wali = $_POST['namaibuwali'];
+    $alamat_ayah_wali = $_POST['alamatayahwali'];
+    $alamat_ibu_wali = $_POST['alamatibuwali'];
+    $telepon_wali = $_POST['teleponwali'];
+    $pekerjaan_ayah_wali = $_POST['pekerjaanayahwali'];
+    $pekerjaan_ibu_wali = $_POST['pekerjaanibuwali'];
+    $pendidikan_terakhir_ayah_wali = $_POST['pendidikanterakhirayahwali'];
+    $pendidikan_terakhir_ibu_wali = $_POST['pendidikanterakhiribuwali'];
 
-    if(!empty($_POST['sifathal'])) {
-        foreach($_POST['sifathal'] as $check) {
-            if ($sifat_hal_fix == ""){
-                $sifat_hal_fix = $check;
-            } else {
-                $sifat_hal_fix = $sifat_hal_fix . ", " . $check;
-            }
-        }
-    }
+    // if(!empty($_POST['sifathal'])) {
+    //     foreach($_POST['sifathal'] as $check) {
+    //         if ($sifat_hal_fix == ""){
+    //             $sifat_hal_fix = $check;
+    //         } else {
+    //             $sifat_hal_fix = $sifat_hal_fix . ", " . $check;
+    //         }
+    //     }
+    // }
 
-    if($sifat == "lain"){
-        $sifatfix = $sifat2;
-    } else {
-        $sifatfix = $sifat;
-    }
+    // if($sifat == "lain"){
+    //     $sifatfix = $sifat2;
+    // } else {
+    //     $sifatfix = $sifat;
+    // }
 
-    if($sifat_hal2c == "lain"){
-        if ($sifat_hal_fix == ""){
-            $sifat_hal_fix = $sifat_hal2;    
-        } else {
-            $sifat_hal_fix = $sifat_hal_fix . ", " . $sifat_hal2;
-        }
-    }
+    // if($sifat_hal2c == "lain"){
+    //     if ($sifat_hal_fix == ""){
+    //         $sifat_hal_fix = $sifat_hal2;    
+    //     } else {
+    //         $sifat_hal_fix = $sifat_hal_fix . ", " . $sifat_hal2;
+    //     }
+    // }
 
     // echo $surat_dari."<br>";
     // echo $no_surat."<br>";
@@ -64,10 +85,16 @@
     // echo $hal."<br>";
     // echo $sifat_hal_fix."<br>";
     
-    $sql = "INSERT INTO disposisi (id, suratdari, nosurat, tglsurat, tglditerima, noagenda, sifat, perihal, sifathal, diteruskan, harapan, catatan, status) VALUES ('', '$surat_dari', '$no_surat', '$tanggal_surat', '$diterima_tanggal', '$no_agenda', '$sifatfix', '$hal', '$sifat_hal_fix', '', '', '', 'Di Kabid')";
+    // $sql = "INSERT INTO disposisi (id, suratdari, nosurat, tglsurat, tglditerima, noagenda, sifat, perihal, sifathal, diteruskan, harapan, catatan, status) VALUES ('', '$surat_dari', '$no_surat', '$tanggal_surat', '$diterima_tanggal', '$no_agenda', '$sifatfix', '$hal', '$sifat_hal_fix', '', '', '', 'Di Kabid')";
+    $sql = "INSERT INTO form(id, namalengkap, namapanggilan, nomorindukasal, nisn, ttl, jeniskelamin, agama, anakke, statusanakdlmkeluarga, alamat, teleponhp, dikelas, padatanggal, namasdmiasal, alamatsekolahasal, 
+                            namalengkapayah, namalengkapibu, alamatayah, alamatibu, teleponhportu, pekerjaanayah, pekerjaanibu, pendidikanterakhirayah, pendidikanterakhiribu,
+                            namaayahwali, namaibuwali, alamatayahwali, alamatibuwali, teleponwali, pekerjaanayahwali, pekerjaanibuwali, pendidikanterakhirayahwali, pendidikanterakhiribuwali) VALUES 
+                            ('','$nama_lengkap','$nama_panggilan','$nomor_induk_asal','$nisn','$ttl','$jenis_kelamin','$agama','$anak_ke','$status_anak_dalam_keluarga','$alamat','$telepon_hp','$di_kelas','$pada_tanggal','$nama_sd_mi_asal','$alamat_sekolah_asal',
+                            '$nama_lengkap_ayah','$nama_lengkap_ibu','$alamat_ayah','$alamat_ibu','$telepon_hp_ortu','$pekerjaan_ayah','$pekerjaan_ibu','$pendidikan_terakhir_ayah','$pendidikan_terakhir_ibu',
+                            '$nama_ayah_wali','$nama_ibu_wali','$alamat_ayah_wali','$alamat_ibu_wali','$telepon_wali','$pekerjaan_ayah_wali','$pekerjaan_ibu_wali','$pendidikan_terakhir_ayah_wali','$pendidikan_terakhir_ibu_wali')";
 
     if ($koneksi->query($sql) === TRUE) {
-        echo "<script>alert('Input berhasil');window.location = '../../index.php?module=surat_masuk';</script>";
+        echo "<script>alert('Input berhasil');window.location = '/Web-Sekolah-Az-Zahrah/formulir.php';</script>";
     } else {
         echo "Error: " . $sql . "<br>" . $koneksi->error;
     }
